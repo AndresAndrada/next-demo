392716723a8067ef11e7c88cc757512b13276c83
unknown <andresandrada1994@gmail.com>
2025-10-08 11:02:16 -0300

    Add cart page

---
+++ src/app/payment/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function FavoritePage() {
+  return (
+    <div className={styles.containerFavorite}>
+      <div className={styles.headerFavorite}>
+        <h1>Carrito de compras</h1>
+      </div>
+    </div>
+  );
+}
