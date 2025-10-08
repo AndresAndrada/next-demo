16a619272a377d1cdb6e8754cf168cfaf1cf6d30
unknown <andresandrada1994@gmail.com>
2025-10-07 23:09:19 -0300

    Change the favorite page

--- src/app/favorite/page.tsx
+++ src/app/favorite/page.tsx
@@ -1,6 +1,7 @@
 import FavoriteComponent from "@/module/components/products/favorite/FavoriteComponent";
 import products from "@/lib/articles.json";
 import { Product } from "@/lib/types";
+import styles from "./page.module.css";
 
 export default function FavoritePage() {
   const favorites = (products as Product[]).filter((p) => p.fav === true);
@@ -10,10 +11,12 @@ export default function FavoritePage() {
   }
 
   return (
-    <>
-      {favorites.map((p) => (
-        <FavoriteComponent key={p.id} product={p} />
-      ))}
-    </>
+    <div className={styles.containerFavorite}>
+      <div className={styles.favorites}>
+        {favorites.map((p) => (
+          <FavoriteComponent key={p.id} product={p} />
+        ))}
+      </div>
+    </div>
   );
 }
--- src/module/components/products/favorite/FavoriteComponent.tsx
+++ src/module/components/products/favorite/FavoriteComponent.tsx
@@ -4,9 +4,7 @@ import { scrollToTop } from "@/utils/scrollToTop";
 import { Product } from "@/lib/types";
 import { useEffect } from "react";
 import styles from "./FavoriteComponent.module.css";
-import Image from "next/image";
-import Link from "next/link";
-import toast from "react-hot-toast";
+import ProductCard from "../productCard/ProductCard";
 
 interface FavoriteProps {
   product: Product;
@@ -18,73 +16,10 @@ export default function FavoriteComponent({ product }: FavoriteProps) {
   }, []);
 
   return (
-    <div className={styles.container}>
-      <div className={styles.imageContainer}>
-        <div className={styles.titleContainer}>
-          <h5 className={styles.titleName}>{product.titulo}</h5>
-          <p className={styles.description}>$ {product.precio}</p>
-        </div>
-        <div className={styles.imagewrappert}>
-          <Image
-            src={product.imagen}
-            alt={product.titulo}
-            width={400}
-            height={400}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.descriptionContainer}>
-          <h5 className={styles.titleDescription}>Descripción</h5>
-          <p className={styles.description}>{product.descripcion}</p>
-        </div>
-      </div>
-      <div className={styles.detailsContainer}>
-        <div className={styles.details}>
-          <h1 className={styles.title}>{product.titulo}</h1>
-          <div className={styles.rating}>
-            {"★".repeat(Math.floor(product.rating))}
-            {"☆".repeat(5 - Math.floor(product.rating))}{" "}
-            <span className={styles.ratingNumber}>{product.rating}</span>
-          </div>
-          <p className={styles.price}>
-            Clacificación {product.rating} de 5 estrellas
-          </p>
-          <p className={styles.price}>${product.precio}</p>
-          <div>
-            <p className={styles.detailsItems}>Ver los medios de pago</p>
-            <p className={styles.detailsItems}>Llega gratis hoy</p>
-          </div>
-          <div>
-            <p className={styles.detailsItems}>Mas formas de entrega</p>
-            <p className={styles.detailsItems}>Devolucion gratis</p>
-          </div>
-          <div>
-            <p className={styles.detailsItems}>
-              Tenes 30 días desde que lo recibis.
-            </p>
-          </div>
-          <div>
-            <Link href={"/"} className={styles.detailsLink}>
-              Conocer más
-            </Link>
-          </div>
-        </div>
-        <div className={styles.actions}>
-          <button
-            className={styles.addToCart}
-            onClick={() => {
-              //   addItem(product);
-              toast.success("Producto agregado correctamente", {
-                duration: 2000,
-                position: "top-center",
-              });
-            }}
-            aria-label={`Agregar ${product.titulo} al carrito`}
-          >
-            Comprar
-          </button>
-          {/* <FavoritesButton productId={product.id} /> */}
-        </div>
+    <div className={styles.containerFavorite}>
+      <div className={styles.container}>
+        <div className={styles.productCard}></div>
+        <ProductCard product={product} />
       </div>
     </div>
   );
